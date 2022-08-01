500314ba54e9d301c3c38408543f9723802292ed
Kenny <kenny@e-pay.vip>
2022-08-01 14:24:55 +0800

    test2

--- rpc/internal/logic/genotplogic.go
+++ rpc/internal/logic/genotplogic.go
@@ -34,7 +34,6 @@ func (l *GenOtpLogic) GenOtp(in *otpclient.OtpGenRequest) (*otpclient.OtpGenResp
 	//	Key:   "ccc",
 	//	Value: attribute.StringValue("QQQQQ"),
 	//})
-	//TES
 	if err != nil {
 		return &otpclient.OtpGenResponse{
 			Code:    "1",
